9f296fe84e7d67232c08d78b57ff561fb681b8cd
y4nka320 <gmejiam@estudiante.uniajc.edu.co>
2026-09-01 17:22:16 +0000

    creacion de los arreglos

--- Programacion3/semana2/EjecutarSemanaDos.java
+++ Programacion3/semana2/EjecutarSemanaDos.java
@@ -1,6 +1,31 @@
-public class EjecutarSemanaDos {
-    public static void main(String[] args) {
-        System.out.println("hola");
-        System.out.println("como estan muchachos");
+public class EjecutarSemanaDos 
+{
+    public static void main(String[] args) 
+    {
+       //Declaracion del Arreglos de la forma 1
+
+        int[] a = {3, 10, 5, 9, 11, 22, 1 ,14 ,25 , 7, 8,
+            19};
+            
+            for (int i = 0; i < a.length; i++){
+                System.out.println("a[" + i + "]=" + a[i]);
+            }
+
+        //Declaracion del arreglo de la forma 2
+        int[] b = new int[12];
+
+        int max = 100;
+        int min =1;
+
+        for (int i = 0; i< b.length; i++){
+            b[i]= (int) (Math.random() *(max - min +1)) + min;
+
+        }
+            //Recorrer y mostrar el arreglo
+            
+         for (int i = 0; i < b.length; i++){
+                System.out.println("b[" + i + "]=" + b[i]);
+            }
+
     }
 }
