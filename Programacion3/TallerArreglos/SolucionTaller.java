1b64c05ea40999e25a003692ac9187ec11bc18f5
y4nka320 <gmejiam@estudiante.uniajc.edu.co>
2026-09-09 20:12:39 +0000

    solucione el punto 7 del taller, muestra los numeros pares e impares

--- Programacion3/TallerArreglos/SolucionTaller.java
+++ Programacion3/TallerArreglos/SolucionTaller.java
@@ -5,6 +5,45 @@ public class SolucionTaller
     public static void main (String[] arg)
     {
 
-        System.out.println("hola");
+        int[] a = {2, 3, 7, 6, 12, 27, 21, 33, 24, 18};
+        
+        int[] b = new int[a.length]; 
+        int[] c = new int[a.length];
+
+        int posb = 0;
+        int posc = 0;
+
+        for (int i = 0 ; i < a.length ; i++)
+        {
+
+            if(a[i] % 2 == 0){
+                 b[posb] = a[i];
+                posb++;
+               
+            }else{
+
+                 c[posc] = a[i];
+                posc++;
+
+            }
+               
+
+
+        }
+
+            System.out.println("los numeros pares son:\n");
+
+                for (int i = 0; i < posb; i++)
+                {
+                    System.out.println("b[" + b[i] +"]");
+                }
+
+             System.out.println("los numeros impares son:\n");
+
+                for (int i = 0; i < posc; i++)
+                {
+                    System.out.println("c[" + c[i] +"]");
+                }
+
     }
 }
