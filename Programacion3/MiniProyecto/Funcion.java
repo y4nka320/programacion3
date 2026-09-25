ac7150793db6d5235bf98079933c0898d6c8a255
y4nka320 <gmejiam@estudiante.uniajc.edu.co>
2026-09-25 23:43:14 +0000

    estoy empezando con mi clase funcionn

--- Programacion3/MiniProyecto/Funcion.java
+++ Programacion3/MiniProyecto/Funcion.java
@@ -2,6 +2,16 @@ package Programacion3.MiniProyecto;
 
 public class Funcion
 {
-    
+    private int franjaHoraria; // si es e la franja 1, 2 o 3
+    private Pelicula peliculaAsignada; 
+    private boolean tieneAsignacion;
+
+    public Funcion (int pFranjaHoraria)
+    {
+        franjaHoraria = pFranjaHoraria;
+        peliculaAsignada = null;
+        tieneAsignacion = false;
+    }
+
     
 }
