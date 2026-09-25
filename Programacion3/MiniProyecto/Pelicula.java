e7be7784e12e5df4c93a5237ca71589642b427fb
y4nka320 <gmejiam@estudiante.uniajc.edu.co>
2026-09-25 23:25:54 +0000

    aca acabe los datos de la pelicula nueva que se quiere ingresar, nombre tipo etc

--- Programacion3/MiniProyecto/Pelicula.java
+++ Programacion3/MiniProyecto/Pelicula.java
@@ -31,7 +31,39 @@ public class Pelicula
     {
         scanner.nextLine();
 
-        System.out.println("Nombre de la pelicula: ");
+        System.out.print("Nombre de la pelicula: ");
+        String nombreIngresado = scanner.nextLine();
+
+        System.out.print("Idioma: ");
+        String idiomaIngresado = scanner.nextLine();
+
+        int opcionTipo;
+        do
+        {
+            System.out.println("En que ddormato desea ver la pelicula?: ");
+            System.out.println("1. 35mm");
+            System.out.println("2. 3D");
+            System.out.print("digite su eleccion: ");
+            opcionTipo = scanner.nextInt();
+
+            switch (opcionTipo) 
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("\n °°°Opcion invalida, compruebe su elecion de nuevo°°°");
+                    
+            }
+        } while (opcionTipo != 1 && opcionTipo != 2);
+         
+            String tipoIngresado = opcionTipo == 1 ? "35mm" : "3D";
+
+            System.out.print("cual es la duracion en minutos de la pelicula: ");
+            int duracionIngresada = scanner.nextInt();
+
+            return new Pelicula(nombreIngresado, idiomaIngresado, tipoIngresado, duracionIngresada);
     }
 
 }
