d8fd6f80a974b9b5809d430c3820cb0391bceebd
y4nka320 <gmejiam@estudiante.uniajc.edu.co>
2026-09-25 23:56:15 +0000

    ya me asigna una hora a cada franja

--- Programacion3/MiniProyecto/Funcion.java
+++ Programacion3/MiniProyecto/Funcion.java
@@ -13,5 +13,26 @@ public class Funcion
         tieneAsignacion = false;
     }
 
+    public int getFranjaHoraria(){return franjaHoraria;} 
     
+    public Pelicula getPeliculaAsignada(){return peliculaAsignada;}
+
+    public boolean tieneAsignacion(){return tieneAsignacion;}
+
+    //aca devuelve el horario en texto a cada franja
+
+    public String getHorarioTexto()
+    {
+        String horario;
+        if(franjaHoraria == 1){
+            horario = "14:00 - 16:30";
+        }else if(franjaHoraria == 2){
+            horario = "16:30 - 19:00";
+        }else {
+            horario = "19:00 - 21:00";
+        }
+        return horario;
+    }
+
+
 }
