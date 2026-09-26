e5e3e467438061eef6a8ab6a5f9f1d0373f30f7b
y4nka320 <gmejiam@estudiante.uniajc.edu.co>
2026-09-26 01:48:47 +0000

    hice otra manera para mostrar un menu

--- Programacion3/MiniProyecto/CinemaStar.java
+++ Programacion3/MiniProyecto/CinemaStar.java
@@ -22,13 +22,45 @@ public class CinemaStar
             System.out.println("\n-----CinemaStar-----"
                                 +  "\n 1. crear o ver peliculas disponibles"
                                 + "\n 2. asignar funciones"
-                                + "\n 3. Modulo de ventas"
+                                + "\n 3. Modulo de ventas"                    //despues reisar si lo cambio
                                 + "\n 4. salir"
                                 + "\n seleccione una opcion: "
             );
-            opcion = scanner.nextInt()
-        }
+            opcion = scanner.nextInt();
 
+            switch (opcion) {
+                case 1:
+                    cantidadPeliculas = menuPeliculas(scanner, peliculaRegistrada, cantidadPeliculas);
+                    break;
+                case 2:
+                    menuFunciones(scanner, salas, peliculaRegistrada, cantidadPeliculas);
+                    break;
+                case 3:
+                    menuVentas(scanner, salas);
+                    break;
+                case 4:
+                    System.out.println("\nGracias por si visita a nuestros cines, hasta pronto");
+                    break;
+                default:
+                    System.out.println("\nOpcion no valida, intentar de nuevo");
+            }
+        } while (opcion !=4); 
+
+        scanner.close();
+
+    }
+
+    private static int menuPeliculas(Scanner scanner, Pelicula[] peliculasRegistradas, int cantidadPeliculas)
+    {
+        int opcionSubmenu;
+        do{
+            System.out.println("\n--- Menu de Peliculas ---");
+            System.out.println("1. Registrar Pelicula");
+            System.out.println("2. Ver Peliculas");
+            System.out.println("3. Volver al menu principal");
+            System.out.print("Seleccione una opcion: ");
+            opcionSubmenu = scanner.nextInt();
+        }
     }
     
 }
